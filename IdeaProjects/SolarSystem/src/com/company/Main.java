3f157ba3544a71a67b59736faec89f1ee93cff3c
hmalick4 <hira.s.malick@gmail.com>
2021-11-21 10:35:37 -0600

    solar system project

---
+++ IdeaProjects/SolarSystem/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem andro=new SolarSystem();
+        andro.sun=5;
+        andro.planet=4;
+        andro.moon=1;
+
+    }
+}
